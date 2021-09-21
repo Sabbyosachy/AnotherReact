a6910a9e4c1c5ac5af242c5c928c793317152a8f
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-09-21 19:30:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <A></A>
+    </div>
+  );
+}
+function A(){
+return(
+<h1>Hlw BD</h1>
+
+);
+}
+export default App;
