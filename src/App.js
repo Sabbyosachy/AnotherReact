a43b822f9f51958ed44ee5ae54372f4fad0ff05e
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-09-21 20:32:48 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[steps,setSteps]=useState(0);
+  const increaseSteps=()=>{
+    const countSteps=steps+1;
+    setSteps(countSteps);
+    console.log(steps);
+
+  }
+  useEffect(()=>{
+    console.log(steps);
+  },[steps])
   return (
     <div className="App">
-      <A></A>
+      <h3>My Steps:{steps}</h3>
+      <button onClick={increaseSteps}>Walk</button>
     </div>
   );
-}
-function A(){
-return(
-<h1>Hlw BD</h1>
-
-);
 }
 export default App;
